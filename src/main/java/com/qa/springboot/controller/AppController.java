2e719f42acf68340259c3dc1edfdc6771be87841
lqk <lqk@163.com>
2020-09-28 16:17:17 +0800

    test2

--- src/main/java/com/qa/springboot/controller/AppController.java
+++ src/main/java/com/qa/springboot/controller/AppController.java
@@ -1,5 +1,8 @@
 package com.qa.springboot.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,16 @@ public class AppController {
 	@RequestMapping("/test1")
 	public Object test1(){
 		return "success";
+	}
+	
+	@RequestMapping("/test2")
+	public Object test2(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		int[] arr = new int[Integer.MAX_VALUE/10];
+		for(int i = 0;i<Integer.MAX_VALUE/10;i++){
+			arr[i] = i;
+		}
+		map.put("arr", arr);
+		return map;
 	}
 }
